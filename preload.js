bb5cd6edf2b13ab18a73c09a68b547a597fc1b9d
AGoubs <arnaud@goubier.fr>
2020-06-16 11:41:35 +0200

    Inital commit

---
+++ preload.js
@@ -0,0 +1,9 @@
+// preload.js
+const { ipcRenderer } = require('electron');
+function init() {
+    // add global variables to your web page
+    window.isElectron = true
+    window.ipcRenderer = ipcRenderer
+}
+
+init(); 
