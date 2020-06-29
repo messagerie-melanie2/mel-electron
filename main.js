045ab3ab8aa9df2754ad5e607743f5b7441377dd
ArnaudG <arnaud.goubier@i-carre.net>
2020-06-29 17:05:12 +0200

    Amélioration affichage mail

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const emlformat = require('eml-format');
 app.commandLine.appendSwitch('ignore-certificate-errors');
 
 let mails = [];
+let cols = [];
 
 let win;
 
@@ -34,19 +35,27 @@ ipcMain.on('read_mail_dir', (event, msg) => {
   dialog.showOpenDialog(win, {
     properties: ['openDirectory']
   }).then(result => {
-    
-    let cols = [];
+
     let path = result.filePaths[0];
     fs.readdir(path, (err, files) => {
+      mails = [];
       files.forEach(file => {
         let eml = fs.readFileSync(path + '/' + file, 'utf8');
         emlformat.read(eml, function (error, data) {
           if (error) return console.log(error);
           fs.writeFileSync("sample.json", JSON.stringify(data, " ", 2));
           let date = new Date(data.date);
-          let date_fr = date.toLocaleString('fr-FR', { timeZone: 'UTC' })
+          let date_fr = date.toLocaleString('fr-FR')
           let from = emlformat.getEmailAddress(emlformat.unquotePrintable(data.from.email));
-          let col = { "subject": data.subject, "fromto": from.email, "date": date_fr };
+
+          let from_name;
+          (from.name == undefined) ? from_name = data.from.name : from.name;
+          
+          let from_email;
+          (from.email == undefined) ? from_email = from[1].email : from_email = from.email;
+          
+          (from_name == undefined) ? from_name = from[1].name : "";
+          let col = { "subject": data.subject.replace(/_/g, ' '), "fromto": from_email, "date": date_fr, "name": from_name.replace(/_/g, ' ')};
           cols.push(col);
           mails.push(data);
         });
@@ -65,24 +74,16 @@ ipcMain.on('mail_select', (event, uid) => {
       return
     }
 
+    let mail = mails[uid];    
+    let col = cols[uid];
     
-    let from = "";
-    let mail = mails[uid];
     let html = data.toString();
-    html = html.replace("%%SUBJECT%%", mail.subject);
-    from = emlformat.getEmailAddress(emlformat.unquotePrintable(mail.from.email));
-    html = html.replace("%%FROM%%", from.email);
+    html = html.replace("%%SUBJECT%%", col.subject);
+    html = html.replace("%%FROM_NAME%%", col.name);
+    html = html.replace("%%FROM%%", col.fromto);
     html = html.replace("%%TO%%", mail.to.email);
-
-    html = html.replace("%%DATE%%", mail.date.toLocaleString('fr-FR', { timeZone: 'UTC' }));
-    if (mail.html != undefined) {
-      html = html.replace("%%OBJECT%%", mail.html);
-    }
-    else {
-      
-      html = html.replace("%%OBJECT%%", ('<pre style="white-space: pre-line;">' + mail.text + '</pre>'));
-      console.log(html);
-    }
+    html = html.replace("%%DATE%%", col.date);
+    (mail.html != undefined) ? html = html.replace("%%OBJECT%%", mail.html) : html = html.replace("%%OBJECT%%", ('<pre style="white-space: pre-line;">' + mail.text + '</pre>'));
 
     win.webContents.send('mail_return', html);
   })
