9917821cb6aacb84f16830710b985955ac42d89a
ArnaudG <arnaud.goubier@i-carre.net>
2020-07-03 15:49:09 +0200

    SimpleMail parser

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const { app, BrowserWindow, dialog, ipcMain } = require('electron')
 const fs = require('fs');
 const path = require('path');
 const emlformat = require('eml-format');
+const simpleParser = require('mailparser').simpleParser;
 
 app.commandLine.appendSwitch('ignore-certificate-errors');
 
-let mails = [];
 let cols = [];
+let promises = [];
 
 let win;
 
@@ -29,61 +30,60 @@ async function createWindow() {
   // win.webContents.openDevTools();
 }
 
+let i = -1;
 app.on("ready", createWindow);
 
 ipcMain.on('read_mail_dir', (event, msg) => {
   dialog.showOpenDialog(win, {
     properties: ['openDirectory']
   }).then(result => {
-
     let path = result.filePaths[0];
     fs.readdir(path, (err, files) => {
-      mails = [];
       files.forEach(file => {
         let eml = fs.readFileSync(path + '/' + file, 'utf8');
-        emlformat.read(eml, function (error, data) {
-          if (error) return console.log(error);
-          fs.writeFileSync("sample.json", JSON.stringify(data, " ", 2));
-          let date = new Date(data.date);
-          let date_fr = date.toLocaleString('fr-FR')
-          let from = emlformat.getEmailAddress(emlformat.unquotePrintable(data.from.email));
-
-          let from_name;
-          (from.name == undefined) ? from_name = data.from.name : from.name;
-          
-          let from_email;
-          (from.email == undefined) ? from_email = from[1].email : from_email = from.email;
-          
-          (from_name == undefined) ? from_name = from[1].name : "";
-          let col = { "subject": data.subject.replace(/_/g, ' '), "fromto": from_email, "date": date_fr, "name": from_name.replace(/_/g, ' ')};
-          cols.push(col);
-          mails.push(data);
-        });
+        i++;
+        promises.push(traitementMails(eml, i))
       });
-      win.webContents.send('mail_dir', cols) // send to web page
+      Promise.all(promises)
+        .then((result) => {
+          cols = result;
+          console.log(cols);
+          win.webContents.send('mail_dir', result) // send to web page
+        }).catch((e) => { })
     });
   })
 })
 
+function traitementMails(eml, i) {
+  return new Promise((resolve) => {
+    simpleParser(eml)
+      .then(parsed => {
+        let date = new Date(parsed.date);
+        let date_fr = date.toLocaleString('fr-FR', { timeZone: 'UTC' })
+        resolve({ "id": i, "subject": parsed.subject, "fromto": parsed.from.value[0].address, "date": date_fr, "name": parsed.from.value[0].name, "to": parsed.to.value[0].address, "text": parsed.textAsHtml});
+      })
+      .catch(err => { });
+  })
+}
+
 
 ipcMain.on('mail_select', (event, uid) => {
-
   fs.readFile('template/messagepreview.html', (err, data) => {
     if (err) {
       console.error(err)
       return
     }
 
-    let mail = mails[uid];    
-    let col = cols[uid];
-    
+    let mail = cols[uid];
+
     let html = data.toString();
-    html = html.replace("%%SUBJECT%%", col.subject);
-    html = html.replace("%%FROM_NAME%%", col.name);
-    html = html.replace("%%FROM%%", col.fromto);
-    html = html.replace("%%TO%%", mail.to.email);
-    html = html.replace("%%DATE%%", col.date);
-    (mail.html != undefined) ? html = html.replace("%%OBJECT%%", mail.html) : html = html.replace("%%OBJECT%%", ('<pre style="white-space: pre-line;">' + mail.text + '</pre>'));
+    html = html.replace("%%SUBJECT%%", mail.subject);
+    html = html.replace("%%FROM_NAME%%", mail.name);
+    html = html.replace("%%FROM%%", mail.fromto);
+    html = html.replace("%%TO%%", mail.to);
+    html = html.replace("%%DATE%%", mail.date);
+    html = html.replace("%%OBJECT%%", mail.text)
+    // (mail.html != undefined) ? html = html.replace("%%OBJECT%%", mail.html) : html = html.replace("%%OBJECT%%", ('<pre style="white-space: pre-line;">' + mail.text + '</pre>'));
 
     win.webContents.send('mail_return', html);
   })
